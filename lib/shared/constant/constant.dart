02aa87a348abaa4e371a1f9f21f4a03f056fe401
MohamedHassan <114914228+mhmedhasn@users.noreply.github.com>
2023-02-19 13:59:00 +0200

    Create API manger

--- lib/shared/constant/constant.dart
+++ lib/shared/constant/constant.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 
 class ConstantApp {
   static const String APIKEY = '15a56053ddfe75468381a6025f507331';
+  static const String BASEURL = 'api.openweathermap.org/data/2.5';
   static const String IMAGE ='assets/image/';
 //Weather date
 //https://api.openweathermap.org/data/2.5/weather?appid=15a56053ddfe75468381a6025f507331&q=cairo
---
+++ lib/shared/network/remote/api_manger.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:weather_to_day/models/WeatherResponse.dart';
+import 'package:weather_to_day/shared/constant/constant.dart';
+
+class APIManger {
+  static Dio? dio;
+  String? URl;
+
+  Map<String, dynamic>? queryParameters;
+
+  APIManger({this.URl, this.queryParameters});
+
+  void getWeather({
+    Function()? beforsend,
+    Function(Map<String, dynamic> data)? onSuccess,
+    Function(dynamic error)? onError,
+  }) {
+    dio?.get(URl!, queryParameters: queryParameters).then((value) {
+      if (value != null) {
+        onSuccess!(value.data);
+      }
+    }).catchError!((er) => onError!(er));
+  }
+}
+///////////////////////////
+class WeatherService {
+  String city;
+
+  WeatherService(this.city);
+
+  getCutrentWeatherData({
+    Function()? beforsend,
+    Function(WeatherResponse weatherResponse)? onSuccess,
+    Function(dynamic error)? onError,
+  }) {
+    final url = '${ConstantApp.BASEURL}/weather?q=$city&${ConstantApp.APIKEY}';
+    APIManger(URl: url, queryParameters: null).getWeather(
+      beforsend: () => beforsend,
+      onSuccess: (data) => onSuccess!(WeatherResponse.fromJson(data)),
+      onError: (error) => onError!(error),
+    );
+  }
+}
